51a3eb6b7d29b33fef35ce003a5c2116f0c1ae45
Fernando Moya <fmoya97.fm@gmail.com>
2020-03-13 10:08:56 -0300

    AddBackSpring

---
+++ Backend-Lab4/Tp1-Lab4/src/main/java/com/utn/project/Tp1Lab4Application.java
@@ -0,0 +1,13 @@
+package com.utn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1Lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1Lab4Application.class, args);
+	}
+
+}
