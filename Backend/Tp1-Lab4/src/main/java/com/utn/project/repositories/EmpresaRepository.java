26825955de8ecbe1492bf61d91202dacff69d843
alemoya <alejandro5320@gmail.com>
2020-03-15 16:19:46 -0300

    Cambiando imports repository

--- Backend/Tp1-Lab4/src/main/java/com/utn/project/repositories/EmpresaRepository.java
+++ Backend/Tp1-Lab4/src/main/java/com/utn/project/repositories/EmpresaRepository.java
@@ -1,9 +1,5 @@
 package com.utn.project.repositories;
-
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.utn.project.entities.Empresa;
 
 public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
--- Backend/Tp1-Lab4/src/main/java/com/utn/project/repositories/NoticiaRepository.java
+++ Backend/Tp1-Lab4/src/main/java/com/utn/project/repositories/NoticiaRepository.java
@@ -1,9 +1,7 @@
 package com.utn.project.repositories;
-
-import org.springframework.data.repository.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.utn.project.entities.Noticia;
 
-public interface NoticiaRepository extends Repository<Noticia, Integer> {
+public interface NoticiaRepository extends JpaRepository<Noticia, Integer> {
 
 }
