38edc9c9cc5bdd706554209fc56877f94c9d8be1
alemoya <alejandro5320@gmail.com>
2020-03-15 19:59:57 -0300

    Componentes basicos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { IndexComponent } from './components/index/index.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IndexComponent,
+    DetalleComponent,
+    BusquedaComponent
   ],
   imports: [
     BrowserModule
