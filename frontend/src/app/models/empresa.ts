97ddda7996fa8ba3ea46bd7627b00fe5106b1213
Fernando Moya <fmoya97.fm@gmail.com>
2020-03-17 18:06:44 -0300

    createModels

---
+++ frontend/src/app/models/empresa.ts
@@ -0,0 +1,12 @@
+
+export class Empresa {
+  id: number;
+  denominacion: string;
+  telefono: string;
+  horario: string;
+  quienSomos: string;
+  latitud: number;
+  longitud: number;
+  domicilio: string;
+  email: string;
+}
---
+++ frontend/src/app/models/noticia.ts
@@ -0,0 +1,11 @@
+
+export class Noticia {
+  id : number;
+  titulo: string;
+  resumen: string;
+  imagen: string;
+  contenidoHtml: string;
+  publicada: boolean;
+  publicacion: string;
+  idEmpresa: number;
+  }
