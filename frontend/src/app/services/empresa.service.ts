f474ca666bd8c8f5ac55735944372825af3074f1
Fernando Moya <fmoya97.fm@gmail.com>
2020-03-17 18:20:03 -0300

    createEmpresaService

---
+++ frontend/src/app/services/empresa.service.ts
@@ -0,0 +1,40 @@
+import { Empresa } from './../models/empresa';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpresaService {
+  miUrl: string = 'http://localhost:8080/api/v1/empresa/';
+
+  constructor(private http: HttpClient) { }
+
+
+
+  getAll(): Observable<Empresa[]> {
+    return this.http.get<Empresa[]>(this.miUrl);
+  }
+
+
+
+getOne(id: number): Observable < Empresa > {
+  return this.http.get<Empresa>(this.miUrl + id);//lo casteo para que me traiga personas
+}
+
+delete (id: number): Observable < any > { //De tipo number
+  //console.log(id);
+  return this.http.delete(this.miUrl + id);
+}
+
+post(empresa: Empresa): Observable < Empresa > {
+  return this.http.post<Empresa>(this.miUrl, empresa);
+}
+
+put(id: number, empresa: Empresa): Observable < Empresa > {
+  return this.http.put<Empresa>(this.miUrl + id, empresa);
+
+}
+
+}
