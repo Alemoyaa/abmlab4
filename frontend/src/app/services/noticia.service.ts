d11497614d9674fbc2c6503ed4b4127702801cb6
salinas38m <salinas38m@gmail.com>
2020-03-17 23:50:53 -0300

    Rectificación noticia service (metodo obsoleto)

--- frontend/src/app/services/noticia.service.ts
+++ frontend/src/app/services/noticia.service.ts
@@ -1,47 +1,37 @@
-import { PostPayLoad } from './../components/tiny/post-payload';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Empresa } from '../models/empresa';
-import { Noticia } from '../models/noticia';
+import { PostPayLoad } from "./../components/tiny/post-payload";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Empresa } from "../models/empresa";
+import { Noticia } from "../models/noticia";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class NoticiaService {
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  miUrl: string = 'localhost:8080/api/v1/noticia/';
+  miUrl: string = "localhost:8080/api/v1/noticia/";
 
-
-  getAll(): Observable<Empresa[]>{
-
+  getAll(): Observable<Empresa[]> {
     return this.httpClient.get<Empresa[]>(this.miUrl);
   }
 
-  getOne(id: number): Observable < Noticia > {
-    return this.httpClient.get<Noticia>(this.miUrl + id);//lo casteo para que me traiga personas
+  getOne(id: number): Observable<Noticia> {
+    return this.httpClient.get<Noticia>(this.miUrl + id); //lo casteo para que me traiga personas
   }
 
-  delete (id: number): Observable < any > { //De tipo number
+  delete(id: number): Observable<any> {
+    //De tipo number
     //console.log(id);
     return this.httpClient.delete(this.miUrl + id);
   }
 
-    // put noticia?
-  addNoticia(postPayload: PostPayLoad) {
-    return this.httpClient.post(
-      'http://localhost:8080/api/v1/noticia/',
-      postPayload
-    );
-  }
-
-  post(noticia: Noticia): Observable < Noticia > {
+  post(noticia: Noticia): Observable<Noticia> {
     return this.httpClient.post<Noticia>(this.miUrl, noticia);
   }
 
-  put(id: number, noticia: Noticia): Observable < Noticia > {
+  put(id: number, noticia: Noticia): Observable<Noticia> {
     return this.httpClient.put<Noticia>(this.miUrl + id, noticia);
-  
   }
 }
